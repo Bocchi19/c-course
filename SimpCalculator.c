f02b4751cdbfa674bf6f371582abf14bbba64f5d
Bocchi19 <quanghieptong@gmail.com>
2024-05-22 19:29:33 +0700

    [ADD add intial UI for user

---
+++ SimpCalculator.c
@@ -0,0 +1,16 @@
+/*Input: 2 integer (2; 2^32 - 1), 1 operator
+Output: Ket qua gon nhat. (2; 2^32 -1)
+
+2. Tim UCLN
+
+*/
+#include <stdio.h>
+int main () {
+    printf ("\nSimple Calculator");
+    
+    printf ("\n1. Basic Calculation");
+    printf ("\n2. GCD");
+    printf ("\n3/ Exit");
+
+    return 0;
+}
