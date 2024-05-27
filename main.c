1654d8a2f5f2a6a9352181d76d2297182abd54df
Bocchi19 <quanghieptong@gmail.com>
2024-05-27 18:40:15 +0700

    add GCD

--- main.c
+++ main.c
@@ -19,5 +19,28 @@ int main () {
     uint8_t option;
     scanf ("%hhu", &option);
     printf ("\nMode selected: %hhu", option);
+
+    if (option == 1){
+        uint32_t firstNumber, secondNumber;
+        printf ("");
+    }
+    else if (option == 2){
+        uint32_t first_number, second_number;
+        
+        printf ("\nEnter first number: "),
+        scanf ("%u", &first_number);
+        printf ("First number: %u\n", first_number);
+
+        printf ("\nEnter second number: ");
+        scanf ("%u", &second_number);
+        printf ("Second number: %u", second_number);
+
+        uint32_t gcd =(first_number < second_number) ? first_number : second_number;
+        for (;gcd >= 1; gcd--){
+            if (first_number % gcd == 0 && second_number % gcd == 0)
+                break;
+        }
+        printf ("\nGCD(%u, %u) = %u", first_number, second_number, gcd);
+    }
     return 0;
 }
